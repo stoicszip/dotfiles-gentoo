808ff2f85761a43abb97fedc970c096b902362d8
stoics <stoics@tuta.com>
2025-02-28 00:11:59 +0100

    changed the swaybar, changed bash script

--- sway/status.sh
+++ sway/status.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# shows active window in swaybar, could also use .name instead of .app_id.
-windowtitle=$(swaymsg -t get_tree 2>/dev/null | jq -r '..|try select(.focused == true).app_id' 2>/dev/null)
-
 # shows time
 date=$(date +'%X')
 
-echo "$windowtitle | $date "
+# shows active window in swaybar, if app_id == null, than the name will be selected.
+windowtitle=$(swaymsg -t get_tree 2>/dev/null | jq -r '..|try select(.focused == true).app_id' 2>/dev/null)
+windowtitle_name=$(swaymsg -t get_tree 2>/dev/null | jq -r '..|try select(.focused == true).name' 2>/dev/null)
+
+if [ "$windowtitle" == 'null' ]; then
+	echo "$windowtitle_name | $date "
+else
+	echo "$windowtitle | $date "
+fi
